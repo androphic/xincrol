c62eb822e0b40843e18d5f333c63ca57ef71cb83
Androphic <estereos@gmail.com>
2022-02-24 03:56:06 +0200

    Update Xincrol.cpp
    some more optimization

--- Xincrol.cpp
+++ Xincrol.cpp
@@ -5,8 +5,8 @@
 // Copyright   : Your copyright notice
 // Description : Xincrol! algorithm in C++, Ansi-style
 // Unique Random Number Generator. For generation random numbers stream where
-// each number appears only once. Useful for IDs, encryption, visual 
-// effects, Random Forest building etc.
+// each number appears only once. Useful for IDs, encryption, visual
+// effects, Random Forest building, Data fogging and Steganography, Games etc.
 //============================================================================
 
 #include <stdio.h>
@@ -39,11 +39,16 @@ inline unsigned int _reverse_(unsigned int iInput) {
 	return iResult;
 }
 
-inline unsigned int _rol_(unsigned int iInput) {
+inline unsigned int _rol_(unsigned int iInput, unsigned int iDist) {
+	iDist = iDist % INT_BITS;
+	return ((iInput << iDist) | (iInput >> (INT_BITS - iDist)));
+}
+
+inline unsigned int _rol1_(unsigned int iInput) {
 	return ((iInput << 1) | (iInput >> (INT_BITS - 1)));
 }
 
-inline unsigned int _rol_(unsigned int iInput, unsigned int iBits) {
+inline unsigned int _rol1_(unsigned int iInput, unsigned int iBits) {
 	unsigned int iMask = (1 << iBits) - 1;
 	unsigned int iRolBackBits = iBits - 1;
 	return ((iInput << 1) | ((iInput >> iRolBackBits) & 1)) & iMask;
@@ -54,21 +59,21 @@ unsigned int xincrol(unsigned int iIndex1, unsigned iIndex2,
 	unsigned int iResult = 0x5aa55aa5 ^ iIndex1;
 	unsigned int iKey = 0xaa55aa55 ^ _reverse_(iIndex2);
 	for (int i = 0; i < INT_BITS * INT_BITS; ++i) {
-		iResult = _rol_((iResult + i) ^ iKey, iBits);
-		iKey = _rol_(++iKey);
+		iResult = _rol1_((iResult + i) ^ iKey, iBits);
+		iKey = _rol_(++iKey, i);
 	}
 	return iResult;
 }
 
 int main(void) {
 	printf("Xincrol demonstration\n");
-	int iBits = 20;
+	int iBits = 21;
 	unsigned int iSeed = currentTimeMillis();
 	int iTS = currentTimeMillis();
 	for (unsigned int i = 0; i < 1 << iBits; ++i) {
 		unsigned int iResult = xincrol(i, iSeed, iBits);
-		printHex(iResult);
+		printInt(iResult);
 	}
-	printf("Time spent(millis): %d\n", currentTimeMillis() - iTS);
+	printf("Time (millis): %d\n", currentTimeMillis() - iTS);
 	return 0;
 }
